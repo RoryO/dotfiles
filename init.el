c00e757dd27d915a1b0ad9f73c6311f9663f0353
Rory O'Connell <rory.oconnell@grandrounds.com>
2017-05-12 10:29:39 -0700

    add emacs init

---
+++ init.el
@@ -0,0 +1,40 @@
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(use-package evil :ensure t :config (evil-mode 1))
+(use-package solarized-theme :ensure t :config (load-theme 'solarized-dark t))
+(use-package magit :ensure t)
+(use-package web-mode :ensure t)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode nil)
+ '(custom-safe-themes
+   (quote
+    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+ '(inhibit-startup-screen t)
+ '(package-selected-packages (quote (web-mode solarized-theme ##)))
+ '(ring-bell-function (quote ignore))
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(when (eq system-type 'darwin)
+  (set-face-attribute 'default nil :family "Inconsolata")
+  (set-face-attribute 'default nil :height 240)
+)
