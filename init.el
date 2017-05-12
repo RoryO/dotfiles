9247a543dcccef39a819160304e146aeeb502f93
Rory O'Connell <rory.oconnell@grandrounds.com>
2017-05-12 15:56:25 -0700

    add packages

--- init.el
+++ init.el
@@ -13,6 +13,13 @@
 (use-package solarized-theme :ensure t :config (load-theme 'solarized-dark t))
 (use-package magit :ensure t)
 (use-package web-mode :ensure t)
+(use-package flycheck :ensure t :config (global-flycheck-mode))
+(use-package relative-line-numbers :ensure t :config (global-relative-line-numbers-mode))
+(use-package robe :ensure t :config (add-hook 'enh-ruby-mode-hook 'robe-mode))
+(use-package enh-ruby-mode :ensure t :config
+    (add-to-list 'auto-mode-alist
+		 '("\\(?:\\.rb\\|ru\\|rake\\|thor\\|jbuilder\\|gemspec\\|podspec\\|/\\(?:Gem\\|Rake\\|Cap\\|Thor\\|Vagrant\\|Guard\\|Pod\\)file\\)\\'" . enh-ruby-mode))
+)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -24,7 +31,9 @@
    (quote
     ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
  '(inhibit-startup-screen t)
- '(package-selected-packages (quote (web-mode solarized-theme ##)))
+ '(package-selected-packages
+   (quote
+    (robe enh-ruby-mode relative-line-numbers flycheck web-mode solarized-theme ##)))
  '(ring-bell-function (quote ignore))
  '(tool-bar-mode nil))
 (custom-set-faces
