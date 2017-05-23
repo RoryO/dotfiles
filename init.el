bcd4a9be02af6c8ebd62f8752a8075b0aff3ddbb
Rory O'Connell <rory.oconnell@grandrounds.com>
2017-05-23 10:20:11 -0700

    more packages

--- init.el
+++ init.el
@@ -1,8 +1,15 @@
-;;; init --- Startup
+;;; Init --- Startup
 ;;; Commentary:
 ;;; Yields to configuration.org
 ;;; Code:
-(org-babel-load-file "./configuration.org" t)
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(org-babel-load-file "./configuration.org")
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
