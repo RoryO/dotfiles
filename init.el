81c7df2a8ec67fb686020ad34a8497d23f45d40d
Rory OConnell <19547+RoryO@users.noreply.github.com>
2018-02-11 09:19:56 -0800

    Add writegood

--- init.el
+++ init.el
@@ -23,7 +23,7 @@
  '(inhibit-startup-screen t)
  '(package-selected-packages
    (quote
-    (nlinum-relative editorconfig rinari rspec-mode evil-surround evil-org company chruby evil-matchit markdown-mode ag projectile-rails slim-mode company-mode magit evil use-package column-marker yaml-mode auto-complete hemisu-theme helm projectile robe enh-ruby-mode relative-line-numbers flycheck web-mode solarized-theme ##)))
+    (writegood-mode nlinum-relative editorconfig rinari rspec-mode evil-surround evil-org company chruby evil-matchit markdown-mode ag projectile-rails slim-mode company-mode magit evil use-package column-marker yaml-mode auto-complete hemisu-theme helm projectile robe enh-ruby-mode relative-line-numbers flycheck web-mode solarized-theme ##)))
  '(projectile-mode t nil (projectile))
  '(ring-bell-function (quote ignore))
  '(semantic-mode t)
