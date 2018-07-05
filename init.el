30d5c784d36c4854d58754dd747ea24c51d9de9e
Rory O'Connell <19547+RoryO@users.noreply.github.com>
2018-07-04 19:17:41 -0700

    use monokai, update package list, clean up

--- init.el
+++ init.el
@@ -23,12 +23,12 @@
  '(blink-cursor-mode nil)
  '(custom-safe-themes
    (quote
-    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+    ("c3d4af771cbe0501d5a865656802788a9a0ff9cf10a7df704ec8b8ef69017c68" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
  '(global-company-mode t)
  '(inhibit-startup-screen t)
  '(package-selected-packages
    (quote
-    (irony company-irony irony-mode helm-company evil-collection writegood-mode nlinum-relative editorconfig rinari rspec-mode evil-surround evil-org company chruby evil-matchit markdown-mode ag projectile-rails slim-mode company-mode magit evil use-package column-marker yaml-mode auto-complete hemisu-theme helm projectile robe enh-ruby-mode relative-line-numbers flycheck web-mode solarized-theme ##)))
+    (rainbow-delimiters org-plus-contrib monokai-theme company-quickhelp irony company-irony irony-mode helm-company evil-collection writegood-mode nlinum-relative editorconfig rinari rspec-mode evil-surround evil-org company chruby evil-matchit markdown-mode ag projectile-rails slim-mode company-mode magit evil use-package column-marker yaml-mode auto-complete hemisu-theme helm projectile robe enh-ruby-mode relative-line-numbers flycheck web-mode solarized-theme ##)))
  '(projectile-mode t nil (projectile))
  '(ring-bell-function (quote ignore))
  '(semantic-mode t)
