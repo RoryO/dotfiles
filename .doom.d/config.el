82cb9835930fb419a312640afeed997a84b0abd3
Rory OConnell <19547+RoryO@users.noreply.github.com>
2020-02-25 11:54:55 -0800

    emacs doom config

---
+++ .doom.d/config.el
@@ -0,0 +1,55 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; refresh' after modifying this file!
+
+;; These are used for a number of things, particularly for GPG configuration,
+;; some email clients, file templates and snippets.
+(setq
+   user-full-name "John Doe"
+   user-mail-address "john@doe.com"
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. These are the defaults.
+   doom-theme 'doom-material
+
+;; If you intend to use org, it is recommended you change this!
+   org-directory "~/org/"
+
+  ;; If you want to change the style of line numbers, change this to `relative' or
+  ;; `nil' to disable it:
+  display-line-numbers-type t
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+  doom-font (font-spec :family "Consolas" :size 32)
+  company-idle-delay 0.2
+  company-auto-complete t
+  w32-pass-lwindow-to-system nil
+  w32-pass-rwindow-to-system nil
+)
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', where Emacs
+;;   looks when you load packages with `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c g k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
+;; they are implemented.
+(after! projectile (setq projectile-indexing-method 'native))
